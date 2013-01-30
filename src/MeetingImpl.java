88d0042bb0464ab0bfeb616dec128ee874be7c8c
RBennett <rbennett485gmail>
2013-01-30 17:47:30 +0000

    Implementation templates created

---
+++ src/MeetingImpl.java
@@ -0,0 +1,25 @@
+import java.util.Calendar;
+import java.util.Set;
+
+
+public class MeetingImpl implements Meeting {
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/PastMeetingImpl.java
@@ -0,0 +1,31 @@
+import java.util.Calendar;
+import java.util.Set;
+
+
+public class PastMeetingImpl implements PastMeeting {
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
