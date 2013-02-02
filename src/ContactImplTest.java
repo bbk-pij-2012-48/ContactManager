b2954a2424666d13755d5f296610667b35ca7e24
RBennett <rbennett485gmail>
2013-02-02 15:06:50 +0000

    ContactImplTest before added

--- src/ContactImplTest.java
+++ src/ContactImplTest.java
@@ -1,9 +1,16 @@
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
 
 
 public class ContactImplTest {
+	
+	@Before
+	public void buildUp() {
+		ContactImpl demo = new ContactImpl(123456789,"Joseph Bloggs");
+		demo.addNotes("Meeting 1");
+		demo.addNotes("Meeting 2");
+	}
 
 	@Test
 	public void testGetId() {
