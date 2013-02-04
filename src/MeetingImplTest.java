a38654ca5e88eca9b5de2f7d11a022ea9683dfdc
RBennett <rbennett485gmail>
2013-02-04 23:24:45 +0000

    @Before buildup() created

--- src/MeetingImplTest.java
+++ src/MeetingImplTest.java
@@ -1,10 +1,30 @@
+import java.util.Set;
+import java.util.TreeSet;
+
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class MeetingImplTest {
 
+	private MeetingImpl demo;
+	
+	@Before
+	public void buildUp() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(2013, 2, 3);
+		Set<Contact> contacts = new TreeSet<Contact>();
+		Contact contact1 = new ContactImpl(1,"Dave");
+		Contact contact2 = new ContactImpl(2,"Murray");
+		contacts.add(contact1);
+		contacts.add(contact2);
+		demo = new MeetingImpl(123456789, cal, contacts);
+	}
+	
 	@Test
 	public void testGetId() {
 		fail("Not yet implemented");
