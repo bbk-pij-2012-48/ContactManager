40ab51f10ea41722d0afc890670831923951065d
RBennett <rbennett485gmail>
2013-02-05 08:11:22 +0000

    Duplicated methods and attributes removed from Future- and PastMeetingImpl

--- src/FutureMeetingImpl.java
+++ src/FutureMeetingImpl.java
@@ -6,25 +6,6 @@ public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
 
 	public FutureMeetingImpl(int id, Calendar date, Set<Contact> contacts) {
 		super(id, date, contacts);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Calendar getDate() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<Contact> getContacts() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
--- src/PastMeetingImpl.java
+++ src/PastMeetingImpl.java
@@ -8,25 +8,7 @@ public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 		super(id, date, contacts);
 	}
 
-	private int id;
-	private Calendar date;
-	private Set<Contact> contacts;
 	private String notes;
-
-	@Override
-	public int getId() {
-		return id;
-	}
-
-	@Override
-	public Calendar getDate() {
-		return date;
-	}
-
-	@Override
-	public Set<Contact> getContacts() {
-		return contacts;
-	}
 
 	@Override
 	public String getNotes() {
