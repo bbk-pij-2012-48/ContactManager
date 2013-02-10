bee0082963b8b2ad1837fa893c4e605bee7d4e46
RBennett <rbennett485gmail>
2013-02-10 14:16:22 +0000

    More tests written

--- src/ContactManagerImplTest.java
+++ src/ContactManagerImplTest.java
@@ -160,12 +160,80 @@ public class ContactManagerImplTest {
 
 	@Test
 	public void testAddNewPastMeeting() {
-		fail("Not yet implemented");
+		Set<Contact> contacts = new TreeSet<Contact>();
+		contacts.add(new ContactImpl(1, "Joe Bloggs"));
+		Calendar testDate = Calendar.getInstance();
+		testDate.set(1, 2, 2012);
+		String notes = "Notes";
+		demo.addNewPastMeeting(contacts, testDate, notes);	
+		Meeting expected = new PastMeetingImpl(3, testDate, contacts);
+		Meeting output = demo.getMeeting(3);
+		assertEquals(expected, output);
+		
 	}
-
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddNewPastMeetingNoContacts() {
+		Set<Contact> contacts = new TreeSet<Contact>();
+		Calendar testDate = Calendar.getInstance();
+		String notes = "Notes";
+		demo.addNewPastMeeting(contacts, testDate, notes);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddNewPastMeetingIllegalContact() {
+		Set<Contact> contacts = new TreeSet<Contact>();
+		contacts.add(new ContactImpl(1, "Joe Bloggs"));
+		contacts.add(new ContactImpl(20, "Steve Jobs"));
+		Calendar testDate = Calendar.getInstance();
+		String notes = "Notes";
+		demo.addNewPastMeeting(contacts, testDate, notes);	
+	}
+	
+	@Test(expected = NullPointerException.class) 
+	public void testAddNewPastMeetingNullContacts() {
+		Calendar testDate = Calendar.getInstance();
+		String notes = "Notes";
+		demo.addNewPastMeeting(null, testDate, notes);
+	}
+	
+	@Test(expected = NullPointerException.class) 
+	public void testAddNewPastMeetingNullCalendar() {
+		Set<Contact> contacts = new TreeSet<Contact>();
+		contacts.add(new ContactImpl(1, "Joe Bloggs"));
+		String notes = "Notes";
+		demo.addNewPastMeeting(contacts, null, notes);
+	}
+	
+	@Test(expected = NullPointerException.class) 
+	public void testAddNewPastMeetingNullNotes() {
+		Set<Contact> contacts = new TreeSet<Contact>();
+		contacts.add(new ContactImpl(1, "Joe Bloggs"));
+		Calendar testDate = Calendar.getInstance();
+		demo.addNewPastMeeting(contacts, testDate, null);
+	}
+	
 	@Test
 	public void testAddMeetingNotes() {
-		fail("Not yet implemented");
+		String expected = "New notes.";
+		demo.addMeetingNotes(2, expected);
+		String output = ((PastMeeting)demo.getMeeting(2)).getNotes();
+		assertTrue(output.equals(expected));		
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddMeetingNotesIllegalId() {
+		demo.addMeetingNotes(20, "notes");
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testAddMeetingNotesFutureMeeting() {
+		demo.addMeetingNotes(1, "notes");
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testAddMeetingNotesNull() {
+		demo.addMeetingNotes(2, null);
 	}
 
 	@Test
