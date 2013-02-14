58ba91f0833fcbc61ad3af6e8014142d0c03f388
RBennett <rbennett485gmail>
2013-02-14 17:32:38 +0000

    equals(), hashCode(), and compareTo() edited for ContactImpl and MeetingImpl

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -25,21 +25,10 @@ public class MeetingImpl implements Meeting, Comparable<MeetingImpl> {
 	public Calendar getDate() {
 		return date;
 	}
-
+	
 	@Override
 	public Set<Contact> getContacts() {
 		return contacts;
-	}
-
-	@Override
-	public int compareTo(MeetingImpl meeting2) {
-		if(this.getId() == meeting2.getId()) {
-			return 0;
-		}
-		if(this.getId() < meeting2.getId()) {
-			return -1;
-		}
-		return 1;
 	}
 	
 	public boolean attendedBy(Contact contact) {
@@ -54,9 +43,17 @@ public class MeetingImpl implements Meeting, Comparable<MeetingImpl> {
 	}
 
 	@Override
+	public int compareTo(MeetingImpl meeting2) {
+		return this.getDate().compareTo(meeting2.getDate());
+	}
+	
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result
+				+ ((contacts == null) ? 0 : contacts.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
 		result = prime * result + id;
 		return result;
 	}
@@ -70,6 +67,16 @@ public class MeetingImpl implements Meeting, Comparable<MeetingImpl> {
 		if (getClass() != obj.getClass())
 			return false;
 		MeetingImpl other = (MeetingImpl) obj;
+		if (contacts == null) {
+			if (other.contacts != null)
+				return false;
+		} else if (!contacts.equals(other.contacts))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
 		if (id != other.id)
 			return false;
 		return true;
